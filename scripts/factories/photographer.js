2afd9545916b0aa164283ed81049916e5397821e
Skunk-XCP <skunk-xcp@hotmail.fr>
2023-05-15 17:30:27 +0200

    refacto folders, fetch data home page, css home page

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,41 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, id, city, country, tagline, price, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
-
+    
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        article.classList.add("photographer-card");
+        
+        const link = document.createElement( 'a' );
+        link.setAttribute("href", `photographer.html?id=${id}`);
+
+        const profilPic = document.createElement( 'img' );
+        profilPic.setAttribute("src", picture)
+        profilPic.alt= `Photo de ${name}`;
+
+        const nameArtist = document.createElement( 'h2' );
+        nameArtist.textContent = name;
+
+        const living = document.createElement( 'p' );
+        living.textContent = `${city}, ${country}`;
+
+        const catchLine = document.createElement( 'p' );
+        catchLine.textContent = tagline;
+
+        const payment = document.createElement( 'p' );
+        payment.textContent = `${price}€/jour`;
+        
+        article.appendChild(link);
+        link.appendChild(profilPic);
+        link.appendChild(nameArtist);
+        article.appendChild(living);
+        article.appendChild(catchLine);
+        article.appendChild(payment);
+
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, id, city, country, tagline, price, picture, getUserCardDOM }
 }
+
+export { photographerFactory };
